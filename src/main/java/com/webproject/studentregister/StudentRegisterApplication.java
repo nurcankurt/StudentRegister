7f6b6ddd6b8171dc9e3deaef3231db8fb65c078e
Nurcan Kurt <nkurt430@gmail.com>
2022-09-06 09:42:13 +0300

    Initial commit

---
+++ src/main/java/com/webproject/studentregister/StudentRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.webproject.studentregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webproject/studentregister/StudentRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webproject.studentregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
